9c203aa6a8dbbf3f41da3828d091968fbb29c53d
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-04-29 14:44:19 +0300

    Помогите.

---
+++ src/main/java/skypro/homework10course2/Service/ValidatorService.java
@@ -0,0 +1,21 @@
+package skypro.homework10course2.Service;
+
+import org.apache.commons.lang3.StringUtils;
+import skypro.homework10course2.Exception.InvalidFirstNameException;
+import skypro.homework10course2.Exception.InvalidLastNameException;
+
+public class ValidatorService {
+    public String validateFirstName(String firstName){
+        if (!StringUtils.isAlpha(firstName)){
+            throw new InvalidFirstNameException();
+        }
+        return StringUtils.capitalize(firstName);
+
+    }
+    public String validateLastName(String lastName){
+        if (!StringUtils.isAlpha(lastName)){
+            throw new InvalidLastNameException();
+        }
+        return StringUtils.capitalize(lastName);
+    }
+}
